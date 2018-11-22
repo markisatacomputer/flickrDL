e7d83ab1e090b5db34108fd0ca8ad15856f345a6
Mark N <mark.is.at.a.computer@gmail.com>
2018-11-21 21:25:44 -0800

    move toward queue - flickr.js provides class that will load flickr records to individual files and execute processing operations on single records
    3 public methods
    - getAllRecords
    - getRecordMeta
    - getRecordImagefile

--- flickr.js
+++ flickr.js
@@ -7,31 +7,26 @@ const request = require('request-compose').stream
 
 require('dotenv').config()
 
-module.exports = class AssetWorker {
+module.exports = class Flickr {
 
   constructor() {
     this.flickr = new Oauth()
     this.storagePath = process.env.OAUTH_STORAGE_PATH
-    this.processing = false
-    this.assets = []
-    this.processed = []
+    this.logPath = this.storagePath+'/flickr.log'
+    this.records = []
     this.page = 0
     this.pages = 0
     this.per_page = 100
     this.total = 0
+
+    //  Create storage dir if nonexistant
+    if (!fs.existsSync(this.storagePath)){
+      fs.mkdirSync(this.storagePath)
+    }
   }
 
-  async init() {
-    console.log("Getting image data from Flickr API.\n")
-    this.callAssetQuery()
-  }
-
-  storeAssetData(assets) {
-    this.assets = this.assets.concat(assets);
-    this.cycleAssetQuery()
-  }
-
-  async callAssetQuery() {
+  async getAllRecords() {
+    var flickr = this
     //  Call Flickr API
     var res = await this._sendRequest({
       method: 'flickr.people.getPhotos',
@@ -44,90 +39,56 @@ module.exports = class AssetWorker {
     if (typeof(res.photos.photo) !== 'undefined') {
       if (this.pages == 0) this.pages = res.photos.pages
       if (this.total == 0) this.total = Number(res.photos.total)
-      this.storeAssetData(res.photos.photo)
+      this.res.photos.photo.forEach((photo) => {
+        flickr._saveRecord(photo)
+      })
     }
-  }
 
-  cycleAssetQuery() {
-    //  Check if we need to continue querying
+    //  Continue
     if (this.page < this.pages) {
       this.page++
-      this.callAssetQuery()
-
-    //  Otherwise Process results
-    } else if (this.assets.length > 0) {
-      this.total = this.assets.length
-      this.processAssets()
+      this.getAllRecords()
     }
   }
 
-  async processAssets() {
-    this.processing = this.assets.pop()
-    await this.downloadAsset()
-    this.processed.push(this.processing)
-    this.processing = false
-    if (this.assets.length > 0) {
-      this.processAssets()
-    } else {
-      this.finalChecks()
-    }
-  }
+  async getRecordMeta(record) {
+    //  Skip records that already have meta
+    if (fs.existsSync(this.storagePath+'/'+record.id+'.meta.json')) return
 
-  finalChecks() {
-    console.log("Running a final check that all original files have been downloaded.\n")
-
-    var problems = []
-    var flickr = this
-    this.processed.forEach((image) => {
-      var flickrFilename = image.url_o.split('/').pop()
-      var storageFilename = flickr.storagePath+'/'+flickrFilename
-      var jsonFilename = storageFilename+'.json'
-      if (!fs.existsSync(storageFilename) || !fs.existsSync(jsonFilename)) {
-        flickr.push(image)
-      }
-    })
-
-    if (problems.length > 0) {
-      console.log("There are "+problems.length+" out of "+this.total+"that didn't get downloaded.  Here they are:\n")
-      console.log(problems)
-    }
-  }
-
-  async getAssetInfo(pid) {
     var info = await this._sendRequest({
       method: 'flickr.photos.getInfo',
-      photo_id: pid,
+      photo_id: record.id,
     })
     var exif = await this._sendRequest({
       method: 'flickr.photos.getExif',
-      photo_id: pid,
+      photo_id: record.id,
     })
-    return {
+    var context = await this._sendRequest({
+      method: 'flickr.photos.getAllContexts',
+      photo_id: record.id,
+    })
+    var meta = {
       description: info.photo.description._content,
       tags: info.photo.tags,
       notes: info.photo.notes,
       originalformat: info.photo.originalformat,
       dates: info.photo.dates,
-      exif: exif.photo.exif
+      exif: exif.photo.exif,
+      context: context
     }
+
+    //  Get info and write
+    record = Object.assign(record, info)
+    this._saveRecord(record, 'meta')
+
+    return record
   }
 
-  async downloadAsset() {
-    //  Create storage dir if nonexistant
-    if (!fs.existsSync(this.storagePath)){
-      fs.mkdirSync(this.storagePath)
-    }
-
-    //  Get filename and storage path
-    var flickrFilename = this.processing.url_o.split('/').pop()
+  async getRecordImagefile(record) {
+    var flickrFilename = record.url_o.split('/').pop()
     var storageFilename = this.storagePath+'/'+flickrFilename
     //  Skip files that already exist in storage
     if (fs.existsSync(storageFilename)) return
-
-    //  Get info and write
-    var info = await this.getAssetInfo(this.processing.id)
-    info = Object.assign(this.processing, info)
-    await fs.promises.writeFile(storageFilename+'.json', JSON.stringify(info, null, '\t') )
 
     //  Get file
     var original = fs.createWriteStream(this.storagePath+'/'+flickrFilename)
@@ -151,6 +112,46 @@ module.exports = class AssetWorker {
     })
     //    - write
     res.pipe(original)
+
+    //  return path to image file
+    return storageFilename
+  }
+
+  _loadRecords(ext='') {
+    var filepaths = fs.readdirSync(this.assetPath)
+    return filepaths.filter((filePath) => {
+      var match = filepath.search('.'+ext+'.')
+      if (ext.length>0) { return match } else { return !match }
+    })
+  }
+
+  _loadRecord(id, ext='') {
+    var storage = this._getRecordPath(id, ext)
+    var record = JSON.parse( fs.readFileSync(storage) )
+  }
+
+  _saveRecord(record, ext='') {
+    var storage = this._getRecordPath(id, ext)
+    fs.writeFileSync(storage, JSON.stringify(record, null, '\t'))
+  }
+
+  _getRecordPath(id, ext='') {
+    var storage = this.storagePath+'/'+record.id
+    if (ext.length > 0) storage += '.'+ext
+    storage += '.json'
+
+    return storage
+  }
+
+  _log(op) {
+    fs.writeFileSync(
+      this.logPath,
+      JSON.stringify({
+        operation: op,
+        date: Date.now(),
+        total: this.assets.length
+      })
+    )
   }
 
   async _sendRequest(req) {
